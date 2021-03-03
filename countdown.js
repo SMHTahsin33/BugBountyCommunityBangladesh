0752e2693fe8960b391c00899a2481f536b94ab2
Syed Mushfik Hasan Tahsin <60981314+SMHTahsin33@users.noreply.github.com>
2021-03-03 19:15:35 +0600

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,26 @@
+var countdownDate = new Date("Mar 05, 2021 00:00:00").getTime();
+
+function evenT() {
+  var today = new Date().getTime();
+  gap = countdownDate - today;
+
+  var second = 1000;
+  var minute = second * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+
+  var d = Math.floor(gap / day);
+  var h = Math.floor((gap % day) / hour);
+  var m = Math.floor((gap % hour) / minute);
+  var s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+}
+
+setInterval(function () {
+  evenT();
+}, 1000);
+
